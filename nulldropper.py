862ccf1eb5d693aa34e750704d28b5b815de3c66
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 14:59:11 -0800

    Revert!

--- nulldropper.py
+++ nulldropper.py
@@ -1,2 +1,9 @@
 def render(table, params):
-    return table.dropna(how='all', axis='index')
+    buf = StringIO()
+    table.info(buf=buf)
+    s = buf.getvalue()
+    info_values = [re.split("\\s\\s+", x) for x in s.split("\n")]   
+    info_values = [x for x in info_values if len(x) > 1]
+    info_values = [x[0] for x in info_values if x[1].startswith('0 non-null')]
+    df = table.drop(info_values, axis=1)
+    return df
